f8203b0efec517c847c1d9374491b169360268c1
datasman <tasiruhassan@live.com>
2024-08-14 14:09:27 +0100

    End of section 15

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,5 +1,7 @@
 using API.Errors;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -9,8 +11,15 @@ namespace API.Controllers
         private readonly StoreContext _context;
         public BuggyController(StoreContext context)
         {
-            
+
             _context = context;
+        }
+
+        [HttpGet("testauth")]
+        [Authorize]
+        public ActionResult<string> GetSecretText()
+        {
+            return "secret stuff";
         }
 
         [HttpGet("not found")]
@@ -25,29 +34,29 @@ namespace API.Controllers
             return Ok();
         }
 
-         [HttpGet("servererror")]
-         public ActionResult GetServerError()
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
         {
             var thing = _context.Products.Find(42);
             var thingToReturn = thing.ToString();
 
             return Ok();
         }
-    
 
-         [HttpGet("badrequest")]
-         public ActionResult GetBadRequest()
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
         {
             return BadRequest(new ApiResponse(400));
         }
-    
 
-         [HttpGet("badrequest/{id}")]
-         public ActionResult GetNotFoundRequest(int id)
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
         {
             return Ok();
         }
     }
 
-    }
+}
 
